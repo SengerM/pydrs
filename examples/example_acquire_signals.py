7830033e7854162a42fb23de34728f7be025d589
Matias Senger <m.senger@hotmail.com>
2022-03-08 12:47:58 +0100

    Added example

--- examples/example_acquire_signals.py
+++ examples/example_acquire_signals.py
@@ -2,27 +2,26 @@ from pydrs import PythonFriendlyBoard
 import numpy as np
 import plotly.express as px
 import pandas
+from pathlib import Path
 
 board = PythonFriendlyBoard() # Open the connection with the board. Here we assume only one board is connected to PC.
 print(f'Connected with {repr(board.idn)}')
 
-SAMPLING_FREQ = 3.5e9
-TRIGGER_DELAY = 100e-9
 # The following lines are self explanatory :)
-board.set_sampling_frequency(Hz=SAMPLING_FREQ)
+board.set_sampling_frequency(Hz=5e9)
 board.set_transparent_mode('on')
 board.set_input_range(center=0)
 board.enable_trigger(True,False) # Don't know what this line does, it was in the example `drs_exam.cpp`.
 board.set_trigger_source('ch4')
 board.set_trigger_level(volts=-.1)
 board.set_trigger_polarity(edge='falling')
-board.set_trigger_delay(seconds=TRIGGER_DELAY)
+board.set_trigger_delay(seconds=100e-9)
 
-df = pandas.DataFrame() # Here I will store some data to plot later on.
-for n_trigger in range(5):
+df = pandas.DataFrame() # Here I will store the data to plot later on.
+for n_trigger in range(5): # Trigger a few times, not just once.
 	board.wait_for_single_trigger() # Halt the program until the board triggers so then we acquire the data.
 	for n_channel in [1,2,4]:
-		waveform_data = board.get_waveform(n_channel) # This returns data in standard numpy arrays.
+		waveform_data = board.get_waveform(n_channel) # This returns data in standard numpy arrays, as you would probably expect in Python.
 		# Store the data in the data frame to plot later on...
 		temp_df = pandas.DataFrame(waveform_data)
 		temp_df['n_trigger'] = n_trigger
@@ -32,11 +31,13 @@ for n_trigger in range(5):
 
 # Plot...
 fig = px.line(
-	title = f'Sampling frequency = {SAMPLING_FREQ:.2e} Hz, trigger delay = {TRIGGER_DELAY:.2e} s',
+	title = f'Signals acquired with the PSI DRS4 Evaluation Board<br><sup>Using <em>pydrs</em> in Python</sup>',
 	data_frame = df,
 	x = 'Time (s)',
 	y = 'Amplitude (V)',
 	color = 'n_trigger',
 	facet_row = 'n_channel',
 )
-fig.write_html('deleteme.html', include_plotlyjs='cdn')
+plot_file_path = Path.home()/Path('pydrs_test.html')
+fig.write_html(str(plot_file_path), include_plotlyjs='cdn')
+print(f'You can now open {plot_file_path} with your favorite web broser and see the signals :)')
